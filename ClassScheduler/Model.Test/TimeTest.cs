57e37c1eb60f9f3797885a66fc5f6d83ea5b0e68
Hunter Haufler <haufhun@gmail.com>
2017-10-05 17:11:48 -0500

    Crated a time class with a start and end time and created overlap method to check if times overalp

---
+++ ClassScheduler/Model.Test/TimeTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Test
+{
+    [TestClass]
+    public class TimeTest
+    {
+        [TestMethod]
+        public void OverlapOfTwoTimesWillReturnTrueForOverlapsWithMethod()
+        {
+            string s = "1:30";
+            string s2 = "5:30";
+
+            string s3 = "3:30";
+            string s4 = "6:30";
+            var t = new Time(s, s2);
+            var t2 = new Time(s3, s4);
+
+            Assert.IsTrue(t.OverlapsWith(t2));
+        }
+
+        [TestMethod]
+        public void NotOverlapOfTwoTimesWillReturnFalseForOverlapsMethod()
+        {
+            string s = "0:30";
+            string s2 = "6:05";
+
+            string s3 = "6:06";
+            string s4 = "7:30";
+            var t = new Time(s, s2);
+            var t2 = new Time(s3, s4);
+
+            Assert.IsFalse(t.OverlapsWith(t2));
+        }
+
+        [TestMethod]
+        public void TimeWithStartTimeSameAsAnotherTimesEndTimeShouldReturnTrueForOverlap()
+        {
+            string s = "0:30";
+            string s2 = "6:06";
+
+            string s3 = "6:06";
+            string s4 = "7:30";
+            var t = new Time(s, s2);
+            var t2 = new Time(s3, s4);
+
+            var overlapsWith = t.OverlapsWith(t2);
+
+            Assert.IsTrue(overlapsWith);
+        }
+    }
+}
---
+++ ClassScheduler/Model/Time.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Time
+    {
+        private readonly string _startTime;
+
+        private readonly string _endTime;
+
+        public Time(string start, string end)
+        {
+            _startTime = start;
+            _endTime = end;
+        }
+
+        public bool OverlapsWith(Time t)
+        {
+            int c = string.CompareOrdinal(_startTime, t._startTime);
+
+            if (c == 0)
+                return true;
+
+            int c2 = c < 0 ? string.CompareOrdinal(this._endTime, t._startTime) : string.CompareOrdinal(t._endTime, this._startTime);
+
+            return c2 >= 0;
+        }
+    }
+}

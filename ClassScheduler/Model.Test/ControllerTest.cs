ede5b587241b171cc605b8c7ca1185ea589b99a8
Hunter Haufler <haufhun@gmail.com>
2017-10-05 17:13:11 -0500

    Creatd base controller with contructor and AddNewClass method and Checks if  lsit of classes is within credit hour range.

---
+++ ClassScheduler/Model.Test/ControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Test
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        [TestMethod]
+        public void IsTotalCreditHoursWithinRange_ShouldBeWithinRange_WHenTotalCreditHoursIsWithinRange()
+        {
+            Controller c = new Controller();
+           var list  = new List<SchoolClass>();
+            list.Add(new SchoolClass(null,null,3,null));
+            list.Add(new SchoolClass(null,null,5,null));
+            list.Add(new SchoolClass(null,null,3,null));
+            list.Add(new SchoolClass(null,null,3,null));
+           var b = c.IsTotalCreditHoursWithinRange(list, 12, 15);
+            Assert.IsTrue(b);
+        }
+        [TestMethod]
+        public void IsTotalCreditHoursWithinRange_ShouldBeOOR_WhenTotalCreditHoursIsOOR()
+        {
+            Controller c = new Controller();
+            var list = new List<SchoolClass>();
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 7, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            var b = c.IsTotalCreditHoursWithinRange(list, 12, 15);
+            Assert.IsFalse(b);
+        }
+
+        [TestMethod]
+        public void IsTotalCreditHoursWithinRange_ShouldBeWithinRange_WhenTotalCreditHoursIsOnLowerBoundryRanges()
+        {
+            Controller c = new Controller();
+            var list = new List<SchoolClass>();
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            var b = c.IsTotalCreditHoursWithinRange(list, 12, 15);
+            Assert.IsTrue(b);
+        }
+        [TestMethod]
+        public void IsTotalCreditHoursWithinRange_ShouldBeWithinRange_WhenTotalCreditHoursIsOnUpperBoundryRanges()
+        {
+            Controller c = new Controller();
+            var list = new List<SchoolClass>();
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 6, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            list.Add(new SchoolClass(null, null, 3, null));
+            var b = c.IsTotalCreditHoursWithinRange(list, 12, 15);
+            Assert.IsTrue(b);
+        }
+    }
+}
---
+++ ClassScheduler/Model/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Controller
+    {
+        private List<SchoolClass> _classes;
+
+        public Controller()
+        {
+            _classes = new List<SchoolClass>();
+        }
+
+        public void AddNewClass(string className, string teacher, int credits, Dictionary<DayOfWeek, List<Time>> classOptions)
+        {
+            var c = new SchoolClass(className, teacher, credits, classOptions);
+            _classes.Add(c);
+        }
+
+        public bool IsTotalCreditHoursWithinRange(IEnumerable<SchoolClass> c, int min, int max)
+        {
+            var total = c.Sum(u => u.Credits);
+            return Enumerable.Range(min, max+1 - min).Contains(total);
+        }
+    }
+}

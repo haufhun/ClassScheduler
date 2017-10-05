862d7ce0ed9a6af56808f02f0811755f0af246fe
Hunter Haufler <haufhun@gmail.com>
2017-10-04 20:51:39 -0500

    Initial commit

---
+++ ClassScheduler/Model.Test/SchoolClassTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Test
+{
+
+    [TestClass]
+    public class SchoolClassTest
+    {
+        [TestMethod]
+        public void SchoolClassIsNotNull()
+        {
+            var c = new SchoolClass();
+            Assert.IsNotNull(c);
+        }
+    }
+}
